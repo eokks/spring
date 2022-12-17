b68ad6b890dcc02691cdffe907728d22ae5c981b
Алина Зуева <zueva@it41.cube>
2022-12-17 13:21:48 +1200

    springproject

---
+++ demka/src/main/java/com/example/demka/DemkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemkaApplication.class, args);
+	}
+
+}
